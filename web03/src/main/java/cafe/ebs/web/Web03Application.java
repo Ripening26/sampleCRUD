be2f1ba6ec9f0c61c3c927b82e9d3c47a0c42b87
Administrator <Administrator@smart20124>
2019-02-08 10:38:17 +0900

    [임홍근]sampleCRUD 프로젝트 초기화

---
+++ web03/src/main/java/cafe/ebs/web/Web03Application.java
@@ -0,0 +1,14 @@
+package cafe.ebs.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web03Application.class, args);
+	}
+
+}
+
